c1cb4ca408392b17517aa9dadacd7a76c234cfc5
fgomez <gomezcuba>
2024-03-26 08:55:18 +0100

    repeat sims wns3

--- plotLog.py
+++ plotLog.py
@@ -9,54 +9,77 @@ from matplotlib import cm
 
 plt.close('all')
 
-logFileName = "TCP-SBF-PAD4-UNRL.log"
-
-file = open(logFileName)
-
-
-listaSINR = []
-listaTBLER = []
-for ln in file:
-    if ln.find("dB TBLER")>=0:#checkeamos se a linea ten un texto caracteristico do print que nos interesa
+def diggestLogFile(logFileName):
+    listaSINR = []
+    listaTBLER = []
+    file = open(logFileName)
+    recBytes = pd.DataFrame(columns=['rnti','recBytesDL','recBytesUL']).set_index(['rnti'])
+    phyStatsList = []
+    for ln in file:
+        if ln.find("dB TBLER")>=0:#checkeamos se a linea ten un texto caracteristico do print que nos interesa
+            _,_,rnti,_,_,size,_,mcs,_,bitmap,_,rv,_,SINRdB,_,_,TBLER,_,corrupted,_,_,_,_,_,_,mcs_ideal,_=ln.split()
+            phyStatsList.append([int(rnti),int(size),int(mcs),int(bitmap),int(rv),float(SINRdB),float(TBLER),int(corrupted[:-1]),int(mcs_ideal)])
+        if ln.find('The number of DL received bytes for UE ')>=0:
+            rnti=int(ln[ln.find('UE')+3:ln.find(':',ln.find('UE')+3)])
+            pkts=int(ln[ln.find(':')+2:])
+            if rnti in recBytes.index:
+                recBytes.loc[rnti].recBytesDL+=pkts#should never reach this point
+            else:
+                recBytes.loc[rnti]=[pkts,0]
+        if ln.find('The number of UL received bytes for UE ')>=0:
+            rnti=int(ln[ln.find('UE')+3:ln.find(':',ln.find('UE')+3)])
+            pkts=int(ln[ln.find(':')+2:])
+            if rnti in recBytes.index:
+                recBytes.loc[rnti].recBytesUL+=pkts#should never reach this point
+            else:
+                recBytes.loc[rnti]=[0,pkts]
+    file.close()
+    phyStats = pd.DataFrame(data=phyStatsList,columns=['rnti','size','mcs','bitmap','rv','SINRdB','TBLER','corrupted','mcs_ideal'])
+    return (phyStats,recBytes)
     
-        posIniSINR=ln.find("SINR ")+5#onde comeza o numero
-        posFinSINR=ln.find(" dB")#onde comeza o numero
-        SINR = float(ln[posIniSINR:posFinSINR])
-        listaSINR.append(SINR)
-        
-        posIniTBLER=ln.find("TBLER ")+6#onde comeza o numero
-        posFinTBLER=ln.find(" corrupted")#onde comeza o numero
-        TBLER = float(ln[posIniTBLER:posFinTBLER])
-        listaTBLER.append(TBLER)
-        
-    #elif ... : # poderiamos coller datos de varios print de formato coñecido no mesmo for
-    #else: # non facemos nada con lineas de formato descoñecido
-    
-SINR = np.array(listaSINR)
-TBLER = np.array(listaTBLER)
-
+def plotCDFdecorate(data,N,text,linfmt='-',clr='b'):
+    [count,vals]=np.histogram(data,bins=N,density=False)
+    CDF=np.cumsum(np.concatenate([[0],count]) / np.sum(count))
+    plt.plot(vals,CDF,linfmt,color=clr,label=text)
 #alternativamente datos = pd.DataFrame(np.array([listaSINR,listaTBLER]).T,columns=("SINR","TBLER"))
 
-[count,BLER]=np.histogram(TBLER,bins=50,density=False)
+phyStats_one,recBytes_one = diggestLogFile('./SICF-ABF-1LAY-UNRL.log')
+phyStats_nosic,recBytes_nosic = diggestLogFile('./SICF-ABF-PAD4-UNRL.log')
+phyStats_sic,recBytes_sic = diggestLogFile('./SICT-ABF-PAD4-UNRL.log')
+# plt.figure(1)
+# plotCDFdecorate(phyStats_one.TBLER,50,"Single Layer",'--o',clr='k')
+# plt.xlabel('BLER')
+# plt.ylabel('C.D.F')
+# plt.axis([0,1,0,1])
 
-CDF=np.cumsum(np.concatenate([[0],count]) / TBLER.size)
+# plt.figure(2)
+# plotCDFdecorate(phyStats_one.SINRdB,50,"Single Layer",'--o',clr='k')
+# plt.xlabel('SINRdB')
+# plt.ylabel('C.D.F')
+# plt.axis([np.min(SINRdB),np.max(SINRdB),0,1])
 
-plt.figure(1)
-plt.plot(BLER,CDF)
-plt.xlabel('BLER')
-plt.ylabel('C.D.F')
-plt.axis([0,1,0,1])
-plt.savefig('BLERCDF.svg')
+plt.figure(3)
+plt.bar(recBytes_one.index,1500/2e-3 *8*1e-6,width=.9,label='Offered Traffic',color=(0,0,0,0),edgecolor=(0,0,0,1),linestyle='dotted')
+Nbar=3
+barwidth=0.9/Nbar
+# for nu in range(Nu):
+nb=0
+offset=(nb-(Nbar-1)/2)*barwidth
+plt.bar(recBytes_one.index+offset,recBytes_one.recBytesUL/1.15 *8*1e-6,width=barwidth,label='Single Layer')
+nb=1
+offset=(nb-(Nbar-1)/2)*barwidth
+plt.bar(recBytes_nosic.index+offset,recBytes_nosic.recBytesUL/1.15 *8*1e-6,width=barwidth,label='4 Layer TIN')
+nb=2
+offset=(nb-(Nbar-1)/2)*barwidth
+plt.bar(recBytes_sic.index+offset,recBytes_sic.recBytesUL/1.15 *8*1e-6,width=barwidth,label='4 Layer SIC')
+plt.xticks(ticks=recBytes_one.index,labels=['U%d'%x for x in recBytes_one.index])
+plt.ylabel('User Achievable rate (Mbps)')
+plt.legend()
+plt.savefig('./rate_vs_user.eps')
 
-[count,SINRdB]=np.histogram(SINR,bins=50,density=False)
+data = pd.read_csv ('UlPdcpStats.txt', delimiter = " ", index_col=False, names = ['mode', 'time', 'txid','rxid','drbid', 'size', 'delay'], engine='python', header=0)
 
-CDF=np.cumsum(np.concatenate([[0],count]) / SINR.size)
+grouped=data[data['mode']=='Rx'].groupby('rxid').delay.mean()
 
-plt.figure(2)
-plt.plot(SINRdB,CDF)
-plt.xlabel('SINRdB')
-plt.ylabel('C.D.F')
-plt.axis([np.min(SINRdB),np.max(SINRdB),0,1])
-
-plt.savefig('SINRCDF.svg')
-# plt.show()
+plt.figure(4)
+plt.bar(grouped.index,grouped*1e-6)
--- plotTrace.py
+++ plotTrace.py
@@ -7,35 +7,40 @@ from matplotlib import cm
 
 plt.close('all')
 
-traceFile = "test.trace"
+traceData_one = pd.read_csv('./SICF-ABF-1LAY-UNRL.trace',sep='\t',header=(0))
+traceData_nosic = pd.read_csv('./SICF-ABF-PAD4-UNRL.trace',sep='\t',header=(0))
+traceData_sic = pd.read_csv('./SICT-ABF-PAD4-UNRL.trace',sep='\t',header=(0))
+nMaxLayer_nosic = np.max(traceData_nosic.RxLayer)
+nMaxLayer_sic = np.max(traceData_sic.RxLayer)
+nMaxLayer = np.maximum( nMaxLayer_nosic, nMaxLayer_sic)
 
-traceData = pd.read_csv('test.trace',sep='\t',header=(0))
-traceData2 = pd.read_csv('testIA.trace',sep='\t',header=(0))
-
-[count,BLER]=np.histogram(traceData.TBler,bins=50,density=False)
-[count2,BLER2]=np.histogram(traceData2.TBler,bins=50,density=False)
-
-CDF=np.cumsum(np.concatenate([[0],count]) / traceData.TBler.size)
-CDF2=np.cumsum(np.concatenate([[0],count2]) / traceData2.TBler.size)
+def plotCDFdecorate(data,N,text,linfmt='-',clr='b'):
+    [count,vals]=np.histogram(data,bins=N,density=False)
+    CDF=np.cumsum(np.concatenate([[0],count]) / np.sum(count))
+    plt.plot(vals,CDF,linfmt,color=clr,label=text)
 
 plt.figure(1)
-plt.plot(BLER,CDF,label="normal")
-plt.plot(BLER2,CDF2,label="IA")
+plotCDFdecorate(traceData_one[(traceData_one.iloc[:,0]=='UL')].TBler,50,"Single Layer",'--o',clr='k')
+for l in range(nMaxLayer_nosic+1):
+    plotCDFdecorate(traceData_nosic[(traceData_nosic.iloc[:,0]=='UL') & (traceData_nosic.RxLayer==l)].TBler,50,"TIN $\ell=%d$"%(l),':x',clr=cm.rainbow_r(l/(nMaxLayer+1)))
+    
+for l in range(nMaxLayer_sic+1):
+    plotCDFdecorate(traceData_sic[(traceData_sic.iloc[:,0]=='UL') & (traceData_sic.RxLayer==l)].TBler,50,"SIC $\ell=%d$"%(l),'-',clr=cm.rainbow_r(l/(nMaxLayer+1)))
 plt.legend()
 plt.xlabel('BLER')
 plt.ylabel('C.D.F')
 plt.axis([0,1,0,1])
-
-[count,SINRdB]=np.histogram(traceData['SINR(dB)'],bins=50,density=False)
-[count2,SINRdB2]=np.histogram(traceData2['SINR(dB)'],bins=50,density=False)
-
-CDF=np.cumsum(np.concatenate([[0],count]) / traceData.TBler.size)
-CDF2=np.cumsum(np.concatenate([[0],count2]) / traceData2.TBler.size)
+plt.savefig('./cdfBLERSIC.eps')
 
 plt.figure(2)
-plt.plot(SINRdB,CDF,label="normal")
-plt.plot(SINRdB2,CDF2,label="IA")
+plotCDFdecorate(traceData_one[(traceData_one.iloc[:,0]=='UL')]['SINR(dB)'],50,"Single Layer",'--o',clr='k')
+for l in range(nMaxLayer_nosic+1):
+    plotCDFdecorate(traceData_nosic[(traceData_nosic.iloc[:,0]=='UL') & (traceData_nosic.RxLayer==l)]['SINR(dB)'],50,"TIN $\ell=%d$"%(l),':x',clr=cm.rainbow_r(l/(nMaxLayer+1)))
+    
+for l in range(nMaxLayer_sic+1):
+    plotCDFdecorate(traceData_sic[(traceData_sic.iloc[:,0]=='UL') & (traceData_sic.RxLayer==l)]['SINR(dB)'],50,"SIC $\ell=%d$"%(l),'-',clr=cm.rainbow_r(l/(nMaxLayer+1)))
 plt.legend()
-plt.xlabel('SINRdB')
+plt.xlabel('SINR (dB)')
 plt.ylabel('C.D.F')
-plt.axis([np.min(SINRdB),np.max(SINRdB),0,1])
+plt.axis([-10,50,0,1])
+plt.savefig('./cdfSINRSIC.eps')
